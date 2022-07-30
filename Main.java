f70d0bf15e9244c73dcb5a4c2e736331bc056bb0
Matthew Guizar <matthewrguizar@gmail.com>
2022-07-29 17:11:15 -0700

    adding comments

--- Main.java
+++ Main.java
@@ -8,11 +8,25 @@ public class Main{
     }
 
     public static ArrayList<Integer> FindLuckyNumbers(int n){
+        /*creating an two array lists one to hold the only 1-10
+        the second will be empty and going to be filled in later
+         */
        ArrayList<Integer> possibleNumbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
        ArrayList<Integer> luckyNumbers = new ArrayList<>();
 
+       /*
+        * creating a loop thats going to run as much as the argument passed in (n times)
+        */
        for (int i = 0; i < n; i++) {
+        /*creating a random number from the length or size of possibleNumbers (10)
+        so essentially getting a random number 1-10. 
+        Math.floor rounds it to the nearest whole number but makes it a double
+        so I cast it to an int
+        */
         int x = (int)Math.floor(Math.random() * possibleNumbers.size());
+        /* adding the random number to luckyNumbers
+         * since the loop runs the amount of n-times it will keep adding numbers based on the argument passed in.
+         */
         luckyNumbers.add(x);
         
        }
